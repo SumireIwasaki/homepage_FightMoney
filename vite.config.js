93eb6646b2a2e89d4f1a9dae9ee08c4affcb5eb6
SumireIwasaki <i224t023@st.kyoto-seika.ac.jp>
2026-07-20 16:22:11 +0900

    Fix Vite config and add deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  base: "/homepage_FightMoney/",
+
   build: {
     rollupOptions: {
       input: {
